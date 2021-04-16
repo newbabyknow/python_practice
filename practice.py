c6d4ffc6d95a4ad5c3a62171bb4717f292cc07fb
linxiaohong_6 <linxiaohong_6@163.com>
2021-04-16 16:08:37 +0800

    remove useless code

--- practice.py
+++ practice.py
@@ -7,11 +7,6 @@ def last_len(str1):
     print(len(list_word[-1]))
 
 
-def num(str1, str2):
-    # 输入一个字符串，一个字母，输出字符串中该字母出现的次数
-    print(str1.count(str2))
-
-
 def sort(str1):
     # 字符串切片方式实现反向输出
     print(str1[::-1])
