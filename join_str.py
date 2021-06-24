aae5a1e3b5774176df10de5146e570667dc4924b
linxiaohong_6 <linxiaohong_6@163.com>
2021-06-24 16:59:13 +0800

    update function join list

--- join_str.py
+++ join_str.py
@@ -1,17 +1,24 @@
 def join_list(m, n):
-    # 合并有序字符串
-    index = 0
-    for i in m:
-        for n_son in range(index, len(n)):
-            print(index)
-            if i < n[n_son]:
-                n.insert(n_son, i)
-                index = n_son + 1
-                break
+    # 合并有序数组
+    if len(n) == 0:
+        return m
+    else:
+        # 把n插入到m里
+        i, j = 0, 0
+        while i < len(m) and j < len(n):
+            if n[j] <= m[i]:
+                m.insert(i, n[j])
+                i += 1
+                j += 1
+            else:
+                i += 1
+        if j < len(n):
+            m += n[j:]
+
+        return m
 
 
 if __name__ == '__main__':
-    list_1 = [1, 2, 3, 4]
-    list_2 = [2, 3, 4, 5, 6]
-    join_list(list_1, list_2)
-    print(list_2)
+    m = [1, 2, 3, 4]
+    n = [1, 2, 3, 4, 7, 8, 9]
+    print(join_list(m, n))
